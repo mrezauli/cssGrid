e0c6729e5cd219c15270892b6b42c3321080270a
MD. Rezaul Islam <rezaul.cse.mbstu@gmail.com>
2020-03-20 23:08:25 +0600

    repo init

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("demo").innerHTML = "Hello World!";
+document.getElementByClass("demo").innerHTML = "Hello World!";
+document.getElementById("demo").innerHTML = "Hello World!";
